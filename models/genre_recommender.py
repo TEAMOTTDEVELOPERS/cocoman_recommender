4a42d79601ac4526d536914f1ce04edc66794d71
als95 <37828653+als95@users.noreply.github.com>
2021-02-24 16:40:48 +0900

    feat: Implement example that recommend using genre (#2)

---
+++ models/genre_recommender.py
@@ -0,0 +1,28 @@
+import numpy as np
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.metrics.pairwise import cosine_similarity
+
+from data.netflix import NetflixData
+
+
+class GenreRecommender:
+    def __init__(self, corpus):
+        self.tf_vectorizer = TfidfVectorizer(analyzer='word', stop_words='english', lowercase=True)
+        self.vectors = self.tf_vectorizer.fit_transform(corpus)
+
+    def recommend(self, query, top_k=20):
+        query_vec = self.tf_vectorizer.transform([query])
+        cosine_sim = cosine_similarity(self.vectors, query_vec).flatten()
+        recommend_indices = np.argsort(cosine_sim, axis=0)[-top_k - 1: -1]
+        return recommend_indices
+
+
+if __name__ == '__main__':
+    netflix = NetflixData()
+    document = netflix.data['description']
+
+    recommender = GenreRecommender(document)
+    indices = recommender.recommend('blood dark humor')
+
+    for i in indices:
+        print(netflix.data['title'][i])
